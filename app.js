784ddb05516498dff66c62bb67a638b6c1339e85
Сергеєв Дмитро <sergeevdima017@gmail.com>
2023-01-24 20:53:58 +0200

    js02

---
+++ app.js
@@ -0,0 +1,96 @@
+'use strict'
+
+// let Name = 'fff'; //string
+// let name;
+
+// +
+// -
+// *
+// %
+// **
+// /
+
+// // a + b;
+// ++ 
+// -- // зменщення значення на 1
+
+// let a = 8; //number
+// let b = 2; 
+
+// let c = a + b * 4;
+
+// alert(c);
+// >
+// <
+// >=
+// <=
+// !=
+
+// == // порівняння
+// === //строге порівнювання 
+
+// let num1 = 5;
+
+// console.log(`значення змінної ${num1 * 3}`);
+
+// console.log(typeof + num1);
+
+// let num1 = prompt('введіть число');
+// let num2 = prompt('введіть число');
+
+// console.log(num1);
+
+// parseInt(prompt(num1));
+// console.log(parseInt(num1));
+
+// let userName = prompt('Як до вас звератися');
+// let age = +prompt('скільки вам років');
+// let rez;
+
+// if (age < 0 || age > 100) {
+//    let rez = 'Веденний не коректний вік';
+//     return;}
+//  else if (age >= 18) {
+//     let rez'привіт' + userName;
+// }
+// else {
+//     let rez'Ще малий';
+// }
+
+// console.log (rez);
+
+
+// const month = +prompt("Введіть номер місяца");
+// let rezz;
+// switch (month) {
+//     case 1: rezz = 'січень'; break;
+//     case 2: rezz = 'лютий'; break;
+//     case 3: rezz = 'березень'; break;
+//     case 4: rezz = 'квітень'; break;
+//     case 5: rezz = 'травень'; break;
+//     case 6: rezz = 'червень'; break;
+//     default:rezz = 'Веденно не правильно'; break;
+// }
+// console.log(rezz);
+
+
+// let a = 5;
+// console.log(a);
+// ++a;
+// // a += 1;
+//  // a = a + 1;
+// console.log(a);
+
+
+let a = +prompt('Будь яке число');
+let rez;
+
+// if(a>0) {
+//     rez = "більше нуля";
+// }
+// else{
+//     rez = 'менше нуля';
+// }
+
+(a>0) ? rez = 'Більше нуля': rez = 'менше нуля';
+console.log(rez);
